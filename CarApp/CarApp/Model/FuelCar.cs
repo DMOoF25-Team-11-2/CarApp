a5d042a13158e0c5663726bbb234642af52743dd
Cecilie <ceciliek1989@outlook.com>
2025-04-22 11:18:53 +0200

    Change to capital F

--- CarApp/CarApp/Model/FuelCar.cs
+++ CarApp/CarApp/Model/FuelCar.cs
@@ -8,7 +8,7 @@ namespace CarApp.Model
 {
     public class FuelCar : Car
     {
-        public double fuelLevel { get; private set; } 
+        public double FuelLevel { get; private set; } 
         public double TankCapacity { get; private set; }
         public double KmPerLiter { get; private set; }
 
