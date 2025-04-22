b8555ce8627ed6534e53566a363aab13fa72e889
Cecilie <ceciliek1989@outlook.com>
2025-04-22 11:03:16 +0200

    Add all properties

--- CarApp/CarApp/Model/Car.cs
+++ CarApp/CarApp/Model/Car.cs
@@ -2,5 +2,22 @@ namespace CarApp.Model;
 
 public class Car
 {
-    
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public string LicensePlate { get; set; }
+    public bool IsEngineOn { get; set; }
+    public int Odometer { get; set; }
+
+   
+
+    public void StartEngine ()
+    { 
+        IsEngineOn = true;
+    }
+    public void StopEngine () 
+    {
+        IsEngineOn = false;
+    }
+    public virtual void Drive (double distance) { } 
+
 }
