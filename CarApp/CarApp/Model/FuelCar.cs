5074ee7869e16651fd390076feed6b7b64e127df
Cecilie <ceciliek1989@outlook.com>
2025-04-22 10:09:12 +0200

    FuelCar tilføjet

---
+++ CarApp/CarApp/Model/FuelCar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApp.Model
+{
+    public class FuelCar : Car
+    {
+        public double fuelLevel { get; private set; } 
+        public double TankCapacity { get; private set; }
+        public double KmPerLiter { get; private set; }
+
+        public FuelCar()  { }
+
+        public void Refuel (double amount) { }
+
+        public override void Drive (double distance) { }
+
+
+    }
+}
