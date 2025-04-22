892b7470da3a288054c417f15d752082f3ebc24e
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-04-22 19:06:22 +0800

    Add test

--- CarApp/CarApp/Program.cs
+++ CarApp/CarApp/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace CarApp
+namespace CarApp;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
---
+++ CarApp/TestProject1/Test1.cs
@@ -0,0 +1,9 @@
+namespace TestProject1;
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
