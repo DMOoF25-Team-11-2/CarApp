37136868b79831a4c39c227ad4f5d271114618c4
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-04-24 17:12:14 +0800

    Use Car abstract class

--- CarApp/CarApp/Model/Taxi.cs
+++ CarApp/CarApp/Model/Taxi.cs
@@ -1,55 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace CarApp.Model;
 
-    class Taxi : Car
+class Taxi : Car
+{
+    public double StartPrice { get; set; }
+    public double PricePerKm { get; set; }
+    public double PricePerMinute { get; set; }
+    public bool MeterStarted { get; set; }
+
+    public Taxi(string brand, string model, string licensePlate, double startPrice, double pricePerKm, double pricePerMinute)
+        : base(brand, model, licensePlate)
     {
-        public double StartPrice { get; set; }
-        public double PricePerKm { get; set; }
-        public double PricePerMinute { get; set; }
-        public bool MeterStarted { get; set; }
+        StartPrice = startPrice;
+        PricePerKm = pricePerKm;
+        PricePerMinute = pricePerMinute;
+        MeterStarted = false;
+    }
 
-        public void StartMeter()
+    public void StartMeter()
+    {
+        MeterStarted = true;
+    }
+
+    public void StopMeter()
+    {
+        MeterStarted = false;
+    }
+
+    public double CalculateFare(double distance, double time)
+    {
+        if (MeterStarted)
         {
-            MeterStarted = true;
+            return StartPrice + (PricePerKm * distance) + (PricePerMinute * time);
         }
-
-        public void StopMeter()
-        {
-            MeterStarted = false;
-        }
-
-        public double CalculateFare(double distance, double time)
-        {
-            if (MeterStarted)
-            {
-                return StartPrice + (PricePerKm * distance) + (PricePerMinute * time);
-            }
-            else
-            {
-                return 0;
-            }
-        }
-
-        public override bool CanDrive()
-        {
-            return IsEngineOn && (MeterStarted || Odometer > 0);
-        }
-
-        public override void UpdateEnergyLevel(double distance)
-        {
-        
-        }
-
-        public override double CalculateConsumption(double distance)
+        else
         {
             return 0;
         }
     }
+
+    public override bool CanDrive(double km)
+    {
+        // Assuming taxis can always drive if the engine is running
+        return IsEngineRunning;
+    }
+
+
+
+
+}
 
 
 
