4ddfeee4ce9b6d3e3bd32291f14ff335e097d055
Cecilie <ceciliek1989@outlook.com>
2025-05-15 14:30:49 +0200

    ca

---
+++ CarApp/CarLibrary/Models/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary.Models;
+
+public class Car : IDrivable
+{
+    public string Brand { get; set; }
+
+    public string Model { get; set; }
+
+    public string LicensePlate { get; set; }
+
+    public int Odometer { get; set; }
+
+    public bool IsEngineRunning { get; set; }
+
+
+    public void Drive(double km) 
+    {
+        
+    }
+
+    public bool CanDrive(double km) 
+    {
+        return true;
+    }
+
+
+    public void StartEngine() => IsEngineRunning = true;
+
+    public void StopEngine() => IsEngineRunning = false;
+
+
+    public override string ToString()
+
+    {
+
+        return $"{Brand},{Model},{LicensePlate},{Odometer}";
+
+    }
+}
---
+++ CarApp/CarLibrary/Models/CarRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary.Models;
+
+public class CarRepository : ICarRepository
+{
+    public ICollection<Car> cars;
+    public CarRepository()
+    {
+        cars = [];
+    }
+
+    public void Add(Car car)
+    {
+        cars.Add(car);
+    }
+
+    public void Delete(Car car)
+    {
+        cars.Remove(car);
+    }
+
+    public Car? FindByLicensePlate(string licensePlate)
+    {
+      return cars.Where(c => c.LicensePlate == licensePlate).FirstOrDefault();
+    }
+
+    public IEnumerable<Car> GetAll()
+    {
+        return cars;
+    }
+
+    public void Update(Car car)
+    {
+        cars.Where(c => c.Equals(car)).FirstOrDefault();
+    }
+
+   
+}
